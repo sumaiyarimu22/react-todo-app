92c777300b5e472db2a2334d4cff24c0a04d9cad
sumaiya <sumaiyarimu22@gmail.com>
2022-12-25 00:38:15 +0600

    end

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,16 +1,21 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FiEdit, FiTrash } from "react-icons/fi";
 import { deleteHandlerContext, editHandlerContext } from "../App";
 
 const Item = ({ task, handleEditSubmitter, editedText, setEditedText }) => {
   const handleDelete = useContext(deleteHandlerContext);
   const handleEdit = useContext(editHandlerContext);
-
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <div className="flex items-center bg-gradient-to-r from-gray-800 to-slate-800 justify-between p-5 rounded hover:from-teal-900 hover:to-gray-800 group">
       <div className="flex gap-3">
         <span>
-          <input type="checkbox" className="accent-teal-400 cursor-pointer" />
+          <input
+            type="checkbox"
+            className="accent-teal-400 cursor-pointer"
+            checked={isChecked}
+            onChange={() => setIsChecked(!isChecked)}
+          />
         </span>
 
         {task.isEditable && (
@@ -26,7 +31,15 @@ const Item = ({ task, handleEditSubmitter, editedText, setEditedText }) => {
         )}
 
         {!task.isEditable && (
-          <p className="group-hover:text-teal-400">{task.text}</p>
+          <p
+            className={`group-hover:text-teal-400 ${
+              isChecked
+                ? `line-through text-gray-500 group-hover:text-teal-600`
+                : null
+            }`}
+          >
+            {task.text}
+          </p>
         )}
       </div>
       <div className="flex gap-3 text-gray-500">
